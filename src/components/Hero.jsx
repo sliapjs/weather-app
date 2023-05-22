a71f6f09e0c7cd06197291ec28e833e736021f17
sliapjs <sliapjs@gmail.com>
2023-05-22 11:07:11 +0300

    add main with styles

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+const Hero = () => {
+  return (
+    <main>
+      <p className="daytime">Good Morning</p>
+      <h2 className="time-display">11:37</h2>
+      <p className="place">In London, UK</p>
+      <button className="btn">More</button>
+    </main>
+  );
+};
+
+export default Hero;
